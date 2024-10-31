2450765958bd40829ee0b389acbf440e97f9a7f1
RednaxMarc <xander.marcle@hotmail.com>
2024-10-31 15:43:58 +0100

    Implemented some graphs in the app (barplots, density and boxplots)

--- Assignment/app.R
+++ Assignment/app.R
@@ -1,8 +1,34 @@
-# Load the Shiny and bslib packages
 library(shiny)
+library(plotly)
 library(bslib)
+library(ggplot2)
+library(gridExtra)
+library(RColorBrewer)
 
-# Define UI for the application with a Minty theme
+## Preprocessing the data
+car_data <- read.csv("Automobile.csv")
+head(car_data)
+str(car_data)
+
+# Check for missing values
+missing_values <- which(is.na(car_data), arr.ind = TRUE)
+print(missing_values)
+print(paste("Column 5 is",colnames(car_data)[5]))
+car_data <- na.omit(car_data)
+
+car_data$model_year <- as.character(car_data$model_year)
+car_data$model_year <- paste0("19",car_data$model_year)
+car_data$cylinders <- as.character(car_data$cylinders)
+car_data$L_100km <- 235.215 / car_data$mpg # Converting mpg to L/100km
+cat_var <- c("name", "brand", "model_year", "origin", "cylinders")
+num_var <- c("L_100km", "displacement", "horsepower", "weight", "acceleration")
+
+# Unique Carbrands
+car_data$brand <- sapply(strsplit(car_data$name, " "), `[`, 1)
+paste0("Unique car models: ",length(unique(car_data$name)),', Unique car brands: ', length(unique(car_data$brand)))
+
+
+## Define UI for the application with a Minty theme
 ui <- fluidPage(
   
   # Apply Minty theme from Bootstrap 5
@@ -31,7 +57,7 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic to switch content based on link selection
+## Define server logic to switch content based on link selection
 server <- function(input, output, session) {
   
   # Create a reactive value to keep track of the selected section
@@ -78,11 +104,93 @@ server <- function(input, output, session) {
         # Render content based on the selected plot type
         output$explore_content <- renderUI({
           if (input$plot_type == "Barplots") {
-            h2("Barplots")
+            output$barplot1 <- renderPlot({
+              par(mfrow = c(1,2))
+              bp <- barplot(table(car_data$origin),
+                            main = "Frequency of each origin",
+                            ylim = c(0,max(table(car_data$origin))+50),
+                            col = brewer.pal(3, "Paired"))
+              text(x=bp, y=table(car_data$origin),label=table(car_data$origin),pos=3)
+              bp <- barplot(table(car_data$cylinders),
+                            main = "Frequency of numb of cylinders",
+                            xlab = "Amount of cylinders",
+                            ylim = c(0,max(table(car_data$cylinders)+20)),
+                            col = brewer.pal(3, "Paired"))
+              text(x=bp, y=table(car_data$cylinders),label=table(car_data$cylinders),pos=3)
+             })
+            output$barplot2 <- renderPlot({
+              par(mfrow = c(1,1))
+              bp <- barplot(table(car_data$model_year),
+                            main = "Frequency of each model year",
+                            ylim = c(0,45),
+                            col = brewer.pal(12, "Set1"),
+                            las = 2)
+              text(x=bp, y=table(car_data$model_year),label=table(car_data$model_year),pos=3)
+            })
+            tabsetPanel(
+              header = h2("Barplots"), 
+              tabPanel("Summary", 
+                       p("Some info here...")),
+              tabPanel("Plots",
+                       # fluidRow(...)
+                       plotOutput("barplot1"),
+                       plotOutput("barplot2")
+              )
+            )
           } else if (input$plot_type == "Density Plots") {
-            h2("Density Plots")
+            output$densityplots <- renderPlot({
+              plot_list <- list()
+              for (var in num_var) {
+                p <- ggplot(car_data, aes(x = !!sym(var))) +
+                  geom_density(fill = "skyblue", color = "black") +
+                  labs(title = paste("Densityplot of", var)) +
+                  theme_minimal() +
+                  theme(plot.title = element_text(hjust = 0.5))
+                plot_list[[length(plot_list) + 1]] <- p
+              }
+              grid.arrange(grobs = plot_list, ncol = 2, nrow = 3)
+            })
+            tabsetPanel(
+              header = h2("Density Plots"), 
+              tabPanel("Summary", 
+                       p("Some info here...")),
+              tabPanel("Plots",
+                       # fluidRow(...)
+                       plotOutput("densityplots", height = 1500)
+              )
+            )
           } else if (input$plot_type == "Boxplots") {
-            h2("Boxplots")
+            output$boxplots <- renderPlot({
+              plot_list <- list()
+              for (i in num_var) {
+                # Making a table with the frequencies of each number of cylinders
+                measurements <- car_data %>%
+                  group_by(cylinders) %>%
+                  summarise(count = sum(!is.na(!!sym(i))))
+                
+                p <- ggplot(car_data, aes(x = as.factor(cylinders), y = !!sym(i))) +
+                  geom_boxplot(fill = "skyblue", color = "black") +
+                  # Adding the frequency for the amount of cylinders
+                  geom_text(data = measurements, aes(label = paste(count, "Measurements"), 
+                                                     x = as.factor(cylinders), 
+                                                     y = max(car_data[[i]], na.rm = TRUE)),
+                            vjust = -1, size = 3) +
+                  labs(title = paste("Cylinders ~", i), x = "Amount of cylinders") +
+                  theme_minimal() + 
+                  theme(plot.title = element_text(hjust = 0.5))
+                plot_list[[length(plot_list) + 1]] <- p
+              }
+              grid.arrange(grobs = plot_list, ncol = 1, nrow = 5)
+            })
+            tabsetPanel(
+              header = h2("Boxplots"), 
+              tabPanel("Summary", 
+                       p("Some info here...")),
+              tabPanel("Plots",
+                       # fluidRow(...)
+                       plotOutput("boxplots", height = 3000)
+              )
+            )
           } else if (input$plot_type == "Correlations") {
             h2("Correlations")
           } else if (input$plot_type == "Violin Plots") {
